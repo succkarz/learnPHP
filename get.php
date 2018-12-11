9e2d79e058c318d6fc3cebc6cc3e315d0577ea63
root <root@localhost.localdomain>
2018-12-11 23:23:05 +0530

    get/post

---
+++ get.php
@@ -0,0 +1,36 @@
+<?php 
+    if(isset($_GET["b1"])){
+        $a = $_GET["t1"];
+        $b = $_GET["t2"];
+        $s = $a + $b;
+        echo "Sum is $s";
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>GET/POST</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body> 
+    <form action="get.php" method="get" name="emp">
+        <table>
+            <tr>
+                <td>Enter first number</td>
+                <td><input type="text" name="t1"/></td>
+            </tr>
+            <tr>
+                <td>Enter second number</td>
+                <td><input type="text" name="t2"/></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" value="Sum" name="b1"/>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,36 @@
+<?php 
+    if(isset($_POST["b1"])){
+        $a = $_POST["t1"];
+        $b = $_POST["t2"];
+        $s = $a + $b;
+        echo "Sum is $s!";
+    }
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>GET/POST</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body> 
+    <form action="post.php" method="post" name="emp">
+        <table>
+            <tr>
+                <td>Enter first number</td>
+                <td><input type="text" name="t1"/></td>
+            </tr>
+            <tr>
+                <td>Enter second number</td>
+                <td><input type="text" name="t2"/></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <input type="submit" value="Sum" name="b1"/>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
